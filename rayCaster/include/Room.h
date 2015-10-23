581adcadab26399e36e256657d970c200f2dedfa
Benjamin Palisse <benjamin.palisse@ensimag.grenoble-inp.fr>
2015-10-23 17:53:40 +0200

    oups

--- rayCaster/include/Room.h
+++ rayCaster/include/Room.h
@@ -4,15 +4,22 @@
 #include "Object.h"
 #define ROOM_H
 
-
-
-
+#include "Ray.h"
+#include "Intersection.h"
+#include <stddef.h>
 
 class Room
 {
     public:
         Room(std::vector<LightSource*> light_container, std::vector<Object*> object_container);
         virtual ~Room();
+
+        std::vector<Object*> getObjectContainer() const;
+
+        void addObject(Object* object);
+
+        // return the intersection of the ray with the first object the rayon will meet in the room
+        Intersection* findIntersection(Ray* ray);
     protected:
         std::vector<LightSource*> light_container;
         std::vector<Object*> object_container;
