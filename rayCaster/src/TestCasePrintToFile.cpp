31a38c611435a5b42534af8b690ff88557735f18
Ikshu Dutta <iksdu821@student.liu.se>
2015-10-24 17:21:09 +0200

    forgot to add unused TestCase for printing to file

---
+++ rayCaster/src/TestCasePrintToFile.cpp
@@ -0,0 +1,17 @@
+#include "TestCasePrintToFile.h"
+
+TestCasePrintToFile::TestCasePrintToFile()
+{
+    //ctor
+}
+
+TestCasePrintToFile::~TestCasePrintToFile()
+{
+    //dtor
+}
+
+void TestCase::run() {
+    TestCase::init();
+
+
+}
