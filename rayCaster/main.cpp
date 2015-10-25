f7c3dd8feb641632fc4723a0239db0ad5509ff23
Ikshu Dutta <iksdu821@student.liu.se>
2015-10-25 17:54:27 +0100

    synch

--- rayCaster/main.cpp
+++ rayCaster/main.cpp
@@ -3,6 +3,7 @@
 #include "TestCaseIntersection2.h"
 #include "TestCaseRayTracer.h"
 #include "TestCaseRayShadowRay.h"
+#include "TestCasePrintToFile.h"
 
 #define NEAR 20
 
@@ -16,10 +17,12 @@ int main()
     TestCase *intersectionTest2 = new TestCaseIntersection2();
     TestCase *raytracerTest = new TestCaseRayTracer();
     TestCase* shadowRayTest = new TestCaseRayShadowRay();
+    TestCase* printTest = new TestCasePrintToFile();
     //intersectionTest->run();
     //intersectionTest2->run();
     //raytracerTest->run();
-    shadowRayTest->run();
+    //shadowRayTest->run();
+    printTest.run();
     return 0;
 
 }
