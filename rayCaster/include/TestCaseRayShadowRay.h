4a37920e293737d5e5b2db9f84ef7ba7a1ae1be2
Benjamin Palisse <benjamin.palisse@ensimag.grenoble-inp.fr>
2015-10-25 17:53:34 +0100

    header of the test...

---
+++ rayCaster/include/TestCaseRayShadowRay.h
@@ -0,0 +1,16 @@
+#ifndef TESTCASERAYSHADOWRAY_H
+#define TESTCASERAYSHADOWRAY_H
+
+#include <TestCase.h>
+
+class TestCaseRayShadowRay: public TestCase
+{
+    public:
+        TestCaseRayShadowRay();
+        virtual ~TestCaseRayShadowRay();
+        void run();
+    protected:
+    private:
+};
+
+#endif // TESTCASERAYSHADOWRAY_H
