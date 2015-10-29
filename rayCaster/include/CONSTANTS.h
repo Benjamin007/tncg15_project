94bd7359a07a324a8b2b9e5dc22ff2d9d1610c86
Benjamin Palisse <benjamin.palisse@ensimag.grenoble-inp.fr>
2015-10-30 00:28:28 +0100

    add a file CONSTANTS.h containing some random constants

---
+++ rayCaster/include/CONSTANTS.h
@@ -0,0 +1,17 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define NBSHADOWRAY 1
+#define SUPER_SAMPLING_NUMBER 16
+#define HEIGHT 1000
+#define WIDTH 1000
+#define MAXBOUNCES 4
+
+// frustum variables:
+#define NEAR -100
+#define LEFT -100
+#define RIGHT 100
+#define TOP 100
+#define BOTTOM -100
+
+#endif
